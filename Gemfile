af9ee7c1be52b792c12fa70b645c59511190626b
Anne Bortoli <annebortoli@gmail.com>
2023-10-08 13:40:15 -0300

    chore: add phone validation

--- Gemfile
+++ Gemfile
@@ -71,3 +71,4 @@ group :test do
 end
 
 gem "cpf_cnpj"
+gem 'phonelib'
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,19 @@ class User < ApplicationRecord
   validates :name, :email, :cpf, :phone, presence: true
   validates :name, length: { minimum: 2 }
   validates :cpf, uniqueness: true
-
+  validates :phone, phone: { possible: true, allow_blank: false }, uniqueness: false
   validates :email, uniqueness: true, format: { with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/ }
 
   validate :validate_cpf_format
+  before_save :phone_format
 
   private
 
   def validate_cpf_format
     errors.add(:cpf, 'is not a valid CPF') unless CPF.valid?(cpf, strict: true)
+  end
+
+  def phone_format
+    self.phone = Phonelib.parse(phone).e164
   end
 end
