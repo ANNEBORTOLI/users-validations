7ccbd96bd3c455780a94546cfa89061dd1fa8a4f
Anne Bortoli <annebortoli@gmail.com>
2023-10-08 10:45:12 -0300

    feat: add validation for cpf

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,13 @@ class User < ApplicationRecord
   validates :name, length: { minimum: 2 }
   validates :cpf, uniqueness: true
 
-  validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :email, uniqueness: true, format: { with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/ }
 
   validate :validate_cpf_format
 
   private
 
   def validate_cpf_format
-    errors.add(:cpf, 'is not a valid CPF') unless CPF.valid?(cpf)
+    errors.add(:cpf, 'is not a valid CPF') unless CPF.valid?(cpf, strict: true)
   end
 end
