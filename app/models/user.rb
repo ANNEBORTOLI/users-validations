6e1c46cc52914bd6606120fadedb87e85e0808f1
Anne Bortoli <annebortoli@gmail.com>
2023-10-11 15:24:19 -0300

    chore: change logo image

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   validates :name, :email, :cpf, :phone, presence: true
   validates :name, length: { minimum: 2 }
   validates :cpf, uniqueness: true
-  validates :phone, phone: { possible: true, allow_blank: false }, uniqueness: false
+  validates :phone, phone: { possible: true, allow_blank: false, types: [:voip, :mobile], countries: :br }, uniqueness: false
   validates :email, uniqueness: true, format: { with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/ }
   validate :valid_cpf?
   before_save :phone_formatter, :email_formatter, :cpf_formatter
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   private
 
   def valid_cpf?
-    errors.add(:cpf, 'is not a valid') unless CPF.valid?(cpf, strict: true)
+    errors.add(:cpf) unless CPF.valid?(cpf, strict: true)
   end
 
   def phone_formatter
